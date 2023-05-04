092a83d1f52a7d20955d719a96c8dfd3f6ba8e0f
Aura-ml <63411078+Aura-ml@users.noreply.github.com>
2023-05-04 11:57:26 +0200

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,20 @@
+                                                                         
+import requests
+import unittest
+
+class TestAPI(unittest.TestCase):
+        def test_requests(self):
+                self.assertEqual(requests.get("http://127.0.0.1:4999/StartingNewProjetFlask/querystring/payload").status_code,200)
+
+        def test_requestsb(self):
+                self.assertEqual(requests.get("http://127.0.0.1:4999/ActiverAudit/querystring/payload/id_projet").status_code,200)
+
+        def test_requestsc(self):
+                self.assertEqual(requests.get("http://127.0.0.1:4999/ExecuterAuditFlask/querystring/id_projet").status_code,200)
+
+        def test_requestsd(self):
+                self.assertEqual(requests.get("http://127.0.0.1:4999/ObtenirRapportFlask/querystring/id_projet").status_code,200)
+
+
+if __name__ == '__main__':
+  unittest.main()
